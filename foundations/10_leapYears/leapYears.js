49e448c32ac971b9e1b9c7527407ea70b74e5765
Jacob Johansson <jacobknuts@gmail.com>
2026-01-09 21:52:12 +0100

    Done with 10_leapYears.

--- foundations/10_leapYears/leapYears.js
+++ foundations/10_leapYears/leapYears.js
@@ -1,6 +1,11 @@
-const leapYears = function() {
+const leapYears = function(year) {
+    //if (year % 400 === 0 && || year % 4 === 0) return true;
+    //return false;
 
+   return year % 4 === 0 && (!(year % 100 === 0) || year % 400 === 0) ? true : false;
 };
+
+console.log(leapYears(700));
 
 // Do not edit below this line
 module.exports = leapYears;
