c49b46dfbac252f2afef1725e89589bf54bcefba
Jacob Johansson <jacobknuts@gmail.com>
2026-01-09 22:11:45 +0100

    Done with 11_tempConversion.

--- foundations/11_tempConversion/tempConversion.js
+++ foundations/11_tempConversion/tempConversion.js
@@ -1,11 +1,20 @@
-const convertToCelsius = function() {
+const convertToCelsius = function (farenheit) {
+	let result = (farenheit - 32) * 5/9;
+	return Number(result.toFixed(1));
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function (celsius) {
+    let result = celsius * 9/5  + 32;
+	return Number(result.toFixed(1));
 };
+
+console.log(convertToCelsius(100));
+console.log(convertToFahrenheit(37.8));
+
+
 
 // Do not edit below this line
 module.exports = {
-  convertToCelsius,
-  convertToFahrenheit
+    convertToCelsius,
+    convertToFahrenheit,
 };
