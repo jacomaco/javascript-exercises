83eb0d0bd905f3619456033f1d738a2b2ee46fcb
Jacob Johansson <jacobknuts@gmail.com>
2026-01-09 20:54:53 +0100

    Done with 08_removeFromArray.

--- foundations/08_removeFromArray/removeFromArray.js
+++ foundations/08_removeFromArray/removeFromArray.js
@@ -1,5 +1,19 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...numsToRemoveFromArray) {
+
+    return array.filter(num => !numsToRemoveFromArray.includes(num));
+    /*
+    let newArray = []; 
+    for (let i = 0; i < array.length; i++) {
+        if (array[i] != number) {
+            newArray.push(array[i]);
+        }
+    }
+    return newArray;
+    */
 };
+
+console.log(removeFromArray([1, 2, 3, 4], 3));
+
 
 // Do not edit below this line
 module.exports = removeFromArray;
