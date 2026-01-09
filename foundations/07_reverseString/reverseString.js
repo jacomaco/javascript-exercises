332cebf59e6c724399db5d845eed118dfc384955
Jacob Johansson <jacobknuts@gmail.com>
2026-01-09 20:04:44 +0100

    Done with 07_reverseString.

--- foundations/07_reverseString/reverseString.js
+++ foundations/07_reverseString/reverseString.js
@@ -1,6 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function(str) {
+    return [...str].reverse().join('');
 };
+
+console.log(reverseString("hello"));
+
 
 // Do not edit below this line
 module.exports = reverseString;
