c5cd4e61ff02cd33f93a441675fb820277c69bd7
Jacob Johansson <jacobknuts@gmail.com>
2026-01-09 21:24:10 +0100

    Done with 09_sumAll.

--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -1,6 +1,17 @@
-const sumAll = function() {
+const sumAll = function (a, b) {
+    if (!Number.isInteger(a) || !Number.isInteger(b)) return "ERROR";
+    if (a < 0 || b < 0) return "ERROR";
+    let lower = Math.min(a, b);
+    let upper = Math.max(a, b);
+    let result = 0;
 
+    for (lower; lower <= upper; lower++) {
+        result += lower;
+    }
+    return result;
 };
+
+console.log(sumAll(1, 4));
 
 // Do not edit below this line
 module.exports = sumAll;
